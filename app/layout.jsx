abe1bdb102c4d7aaaf764b71894c29d5d5ff2fa9
Jimmy <55306720+jmzi@users.noreply.github.com>
2026-06-19 23:13:00 +0800

    add favicon

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,9 +4,9 @@ import './globals.css';
 export const metadata = {
   title: 'Amri – Digital Marketing & Web Development Services',
   icons: {
-    icon: 'https://res.cloudinary.com/dnr4zo1ov/image/upload/v1776558591/logo_kv0csr.png',
-    shortcut:
-      'https://res.cloudinary.com/dnr4zo1ov/image/upload/v1776558591/logo_kv0csr.png',
+    icon: 'https://res.cloudinary.com/dnr4zo1ov/image/upload/w_32,h_32,c_fit/v1776558591/logo_kv0csr.png',
+    shortcut: 'https://res.cloudinary.com/dnr4zo1ov/image/upload/w_32,h_32,c_fit/v1776558591/logo_kv0csr.png',
+    apple: 'https://res.cloudinary.com/dnr4zo1ov/image/upload/w_180,h_180,c_fit/v1776558591/logo_kv0csr.png',
   },
 };
 
@@ -32,6 +32,15 @@ export default function RootLayout({ children }) {
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
             gtag('config', 'G-D9J7GMPNJZ');
+          `}
+        </Script>
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "x9j1u8k16u");
           `}
         </Script>
       </body>
